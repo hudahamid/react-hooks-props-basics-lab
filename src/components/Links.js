8c2bbd58cc218227ec3bed3c85e929e0ed45cb06
hudahamid <huda.00091581@gmail.com>
2022-12-07 21:00:33 +0300

    probs

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Links({ linkedin, github }) { 
+function Links(prob) { 
   return (
     <div id="links">
       
     
       <h3>Links</h3>
-      <a href={github}>{github}</a>
-      <a href={linkedin}>{linkedin}</a>
+      <a href={prob .github}>{prob.github}</a>
+      <a href={prob.linkedin}>{prob.linkedin}</a>
     </div>
   );
 }
