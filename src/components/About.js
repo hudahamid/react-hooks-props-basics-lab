22b31ccd93f49a212e9b7381fe9feda54717dcd9
hudahamid <huda.00091581@gmail.com>
2022-12-07 20:54:39 +0300

    ......

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+function About({bio}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {bio ? <p>{bio}</p> : ""}      
+
+     
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+       
+
       {/* add your <Links /> component here */}
+
+      <Links  linkedin={user.links.linkedin}   github={user.links.github}   />
     </div>
   );
 }
